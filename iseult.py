673f96882431b8f44b54da5d8b369c5fb0445033
Patrick Crumley <patrick.crumley@gmail.com>
2015-11-10 19:10:03 +0100

    Got colorbars working on the phase plot

--- iseult.py
+++ iseult.py
@@ -11,7 +11,7 @@ import numpy as np
 import wx.lib.buttons as buttons
 import  wx.lib.intctrl
 import matplotlib.colors as mcolors
-
+from matplotlib.gridspec import GridSpec
 matplotlib.use('WXAgg')
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_wxagg import \
@@ -435,6 +435,9 @@ class PhasePanel(wx.Window):
         self.norm_type = "PowerNorm"
         self.pow_num = 0.4
 
+        self.show_cbar = True
+        self.weighted = False
+
         self.figure = Figure(figsize=(3, 1), dpi=100)
         self.canvas = FigCanvas(self, -1, self.figure)
         self.Bind(wx.EVT_SIZE, self.sizeHandler)
@@ -478,8 +481,18 @@ class PhasePanel(wx.Window):
             if self.mom_dim == 2:
                 self.y_values = self.Parent.prtl.we
 
-        self.axes = self.figure.add_subplot(111)
-        self.axes.hist2d(self.x_values,self.y_values, bins = [200,200],cmap = new_cmaps.cmaps[self.Parent.cmap], norm = self.norm )
+        self.figure.clf()
+        gs = GridSpec(100,100,bottom=0.1,left=0.1,right=0.9, top = 0.9)
+
+        if self.show_cbar:
+            self.axes = self.figure.add_subplot(gs[20:,:])
+            self.axC = self.figure.add_subplot(gs[:5,:])
+            self.cax = self.axes.hist2d(self.x_values,self.y_values, bins = [200,200],cmap = new_cmaps.cmaps[self.Parent.cmap], norm = self.norm)
+            self.figure.colorbar(self.cax[3], ax = self.axes, cax = self.axC, orientation = 'horizontal')
+        else:
+            self.axes = self.figure.add_subplot(gs[5:,:])
+            self.cax = self.axes.hist2d(self.x_values,self.y_values, bins = [200,200],cmap = new_cmaps.cmaps[self.Parent.cmap], norm = self.norm)
+
         self.canvas.draw()
 
     def onEnter(self, evt):
@@ -489,7 +502,7 @@ class PhasePanel(wx.Window):
         if self.HasCapture():
             self.ReleaseMouse()
     def openGraphPrefs(self, evt):
-        win = PhaseSettings(self, -1, "Chart Settings", size=(350, 200),
+        win = PhaseSettings(self, -1, "Chart Settings",
                           style = wx.DEFAULT_FRAME_STYLE)
         win.Show(True)
     def setKnob(self, value):
@@ -530,42 +543,75 @@ class PhaseSettings(wx.Frame):
         grid1 = wx.GridBagSizer()
 
         # Group of controls for the cmap normalization:
+        self.rbGroupLabel = wx.StaticText(self, label = 'Choose Cmap Norm:') # Title
         self.rbLinear = wx.RadioButton(self, -1, "Linear", style = wx.RB_GROUP)
         self.rbLog = wx.RadioButton(self, -1, "LogNorm" )
         self.rbPow = wx.RadioButton(self, -1, "PowerNorm" )
         self.rbPowNum = wx.TextCtrl(self , -1, str(self.parent.pow_num),
         validator = MyValidator('DIGIT_ONLY') )
-        grid1.Add( self.rbLinear, pos = (0,0), flag = wx.ALIGN_LEFT|wx.LEFT|wx.RIGHT|wx.TOP, border = 5)
-        grid1.Add( self.rbLog, pos = (1,0), flag = wx.ALIGN_LEFT|wx.LEFT|wx.RIGHT|wx.TOP, border = 5)
-        grid1.Add( self.rbPow, pos = (2,0), flag = wx.ALIGN_LEFT|wx.LEFT|wx.RIGHT|wx.TOP, border = 5)
-        grid1.Add( self.rbPowNum, pos = (2,1), flag = wx.ALIGN_LEFT|wx.LEFT|wx.RIGHT|wx.TOP, border = 5)
+
+        # Set the rb that should be selected from the parent
+        if self.parent.norm_type == "Linear":
+            self.rbLinear.SetValue(True)
+        elif self.parent.norm_type == "LogNorm":
+            self.rbLog.SetValue(True)
+
+        else:
+            self.rbPow.SetValue(True)
+
+        # Add to Grid
+        grid1.Add(self.rbGroupLabel, pos = (0,0), span = (1,2))
+        grid1.Add( self.rbLinear, pos = (1,0), flag = wx.ALIGN_LEFT|wx.LEFT|wx.RIGHT|wx.TOP, border = 5)
+        grid1.Add( self.rbLog, pos = (2,0), flag = wx.ALIGN_LEFT|wx.LEFT|wx.RIGHT|wx.TOP, border = 5)
+        grid1.Add( self.rbPow, pos = (3,0), flag = wx.ALIGN_LEFT|wx.LEFT|wx.RIGHT|wx.TOP, border = 5)
+        grid1.Add( self.rbPowNum, pos = (3,1), flag = wx.ALIGN_LEFT|wx.LEFT|wx.RIGHT|wx.TOP, border = 5)
 
         self.Bind(wx.EVT_RADIOBUTTON, self.EvtRadioNorm, self.rbLinear)
         self.Bind(wx.EVT_RADIOBUTTON, self.EvtRadioNorm, self.rbLog)
         self.Bind(wx.EVT_RADIOBUTTON, self.EvtRadioNorm, self.rbPow)
         self.Bind(wx.EVT_TEXT, self.EvtNormNumSet, self.rbPowNum)
 
+        grid.Add(grid1, pos=(2,0), span=(1,2))
+
+        self.cbColorbar = wx.CheckBox(self, -1, "Show Cbar")
+        self.cbColorbar.SetValue(self.parent.show_cbar)
+        self.Bind(wx.EVT_CHECKBOX, self.EvtCheckCbar, self.cbColorbar)
+        grid.Add(self.cbColorbar, pos=(3,0))
+
+        self.cbWeight = wx.CheckBox(self, -1, "Weight")
+        self.cbWeight.SetValue(self.parent.weighted)
+        self.Bind(wx.EVT_CHECKBOX, self.EvtCheckWeight, self.cbWeight)
+        grid.Add(self.cbWeight, pos = (3,1))
+
+
         self.mainsizer.Add(grid,0, border=15)
-        self.mainsizer.Add(grid1, 0, border = 15)
+#        self.mainsizer.Add(grid1, 0, border = 15)
 
         self.SetSizerAndFit(self.mainsizer)
     # Define functions for the events
+
+    def EvtCheckCbar(self, evt):
+        self.parent.show_cbar = evt.IsChecked()
+        self.parent.draw()
+
+    def EvtCheckWeight(self, evt):
+        self.parent.weighted = evt.IsChecked()
+        self.parent.draw()
 
     def EvtRadioDim(self, evt):
         self.parent.mom_dim = evt.GetInt()
         self.parent.draw()
 
     def EvtRadioNorm(self, evt):
-
         self.parent.norm_type = evt.GetEventObject().GetLabel()
         self.parent.draw()
 
     def EvtNormNumSet(self, evt):
         tmp_num = evt.GetString()
         if not tmp_num:
-            tmp_num = 1E-3
-        elif float(tmp_num)<=1E-3:
-            tmp_num = 1E-3
+            tmp_num = 1E-2
+        elif float(tmp_num)<=1E-2:
+            tmp_num = 1E-2
         else:
             tmp_num = float(tmp_num)
         self.parent.pow_num = tmp_num
@@ -767,9 +813,6 @@ class MainWindow(wx.Frame):
             elm.Update(value)
         # refresh the graphs
         self.refreshAllGraphs()
-
-
-
 
     # Define the Main Window functions
     def OnAbout(self,e):
