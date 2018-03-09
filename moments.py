11f434b851ef8d71f2d3c5ff8373f829549f59ed
Patrick Crumley <patrick.crumley@gmail.com>
2018-03-08 23:02:45 -0500

    fixed the temperatures to treat ions and electrons as separate fluids

--- moments.py
+++ moments.py
@@ -229,9 +229,8 @@ class  MomentsPanel:
                     CalcVxEHists(self.xe,self.ue, ge, bin_width, self.xmin,  vex, self.ey, self.ecounts)
                     CalcVxEHists(self.xi,self.ui, gi, bin_width, self.xmin, vix, self.iy, self.icounts)
 
-                    RestFrameBoost(vex, self.ecounts, vix,  self.icounts, vx_avg, boost_gam)
-                    CalcDelGamHists(self.xe, self.ue, self.ve, self.we, ge, vx_avg, boost_gam, bin_width, self.xmin, self.ecounts, self.ex)
-                    CalcDelGamHists(self.xi, self.ui, self.vi, self.wi, gi, vx_avg, boost_gam, bin_width, self.xmin, self.icounts, self.ix)
+                    CalcDelGamHists(self.xe, self.ue, self.ve, self.we, ge, vix, 1/np.sqrt(1-vix**2), bin_width, self.xmin, self.ecounts, self.ex)
+                    CalcDelGamHists(self.xi, self.ui, self.vi, self.wi, gi, ve, 1/np.sqrt(1-vex**2), bin_width, self.xmin, self.icounts, self.ix)
 
 
                 else:
@@ -242,8 +241,8 @@ class  MomentsPanel:
                     CalcVxEWeightedHists(self.xi,self.ui, gi, iweights, bin_width, self.xmin, vix, self.iy, self.icounts)
 
                     RestFrameBoost(vex, self.ecounts, vix,  self.icounts, vx_avg, boost_gam)
-                    CalcDelGamWeightedHists(self.xe, self.ue, self.ve, self.we, ge, eweights, vx_avg, boost_gam, bin_width, self.xmin, self.ecounts, self.ex)
-                    CalcDelGamWeightedHists(self.xi, self.ui, self.vi, self.wi, gi, iweights, vx_avg, boost_gam, bin_width, self.xmin, self.icounts, self.ix)
+                    CalcDelGamWeightedHists(self.xe, self.ue, self.ve, self.we, ge, eweights, vix,  1/np.sqrt(1-vix**2), bin_width, self.xmin, self.ecounts, self.ex)
+                    CalcDelGamWeightedHists(self.xi, self.ui, self.vi, self.wi, gi, iweights, vex,  1/np.sqrt(1-vex**2), bin_width, self.xmin, self.icounts, self.ix)
 
                 self.ex*=self.memi
                 self.ey*=self.memi
