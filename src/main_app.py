4b516fc0f372787eb2ded6b0d4ffb8b0840d59d8
Patrick Crumley <patrick.crumley@gmail.com>
2020-03-26 16:16:42 -0400

    Changed ffmpeg function call

--- src/main_app.py
+++ src/main_app.py
@@ -3330,14 +3330,14 @@ class MainApp(Tk.Tk):
                 self.PrintFig(MakingMovie  = True)
 
             # The ffmpeg command we want to call.
-            # ffmpeg -y -f image2 -framerate 8 -pattern_type glob -i '*.png' -codec copy out.mov
-            # ffmpeg -framerate N -i iseult_img_%03d.png -vf format=yuv420p output.mp4
-
+            ## ffmpeg -framerate [FPS] -i [NAME_***].png -c:v prores -pix_fmt yuv444p10le [OUTPUTNAME].mov
             cmdstring = ['xterm', '-e','ffmpeg',
-                        '-y', '-f', 'image2', # overwrite, image2 is a colorspace thing.
                         '-framerate', str(int(FPS)), # Set framerate to the the user selected option
-                        '-pattern_type', 'glob', '-i', os.path.join(self.movie_dir, '../tmp_erase','*.png'),
-                        '-vf', 'format=yuv420p',  # save as a *.mov
+                        '-i', os.path.join(self.movie_dir, '../tmp_erase','*.png'),
+                        '-c:v',
+                        'prores',
+                        '-pix_fmt',
+                        'yuv444p10le',
                         os.path.join(os.path.join(self.movie_dir,'..'),fname)]#, '&']#, # output name,
                         #'<dev/null', '>dev/null', '2>/var/log/ffmpeg.log', '&'] # run in background
             try:
