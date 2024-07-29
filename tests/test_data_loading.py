b367f9b144dca146d1a2413cea6e998157917f82
Bob Caddy <rcaddy@princeton.edu>
2024-07-29 13:57:39 -0400

    Add skeleton for tests

---
+++ tests/test_data_loading.py
@@ -0,0 +1,25 @@
+import data_loading
+
+def test__detect_tristan_data_version_verify_v1():
+    pass
+
+def test__detect_tristan_data_version_verify_v2():
+    pass
+
+def test__detect_tristan_data_version_incorrect_data():
+    pass
+
+def test__insert_directory_default():
+    pass
+
+def test__insert_directory_specified_location():
+    pass
+
+def test__verify_file_path():
+    pass
+
+def test___handle_tristan_v2():
+    pass
+
+def test_load_dataset():
+    pass
