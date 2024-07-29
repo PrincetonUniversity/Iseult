a9fe24a8ab7f9a40c04907e2a12806f4584fcf90
Bob Caddy <rcaddy@princeton.edu>
2024-07-29 14:19:16 -0400

    Add test for `data_loading.__detect_tristan_data_version`

--- tests/test_data_loading.py
+++ tests/test_data_loading.py
@@ -1,13 +1,37 @@
+import pathlib
+import h5py
+import pytest
 import data_loading
 
+repo_root = pathlib.Path(__file__).resolve().parents[1]
+
 def test__detect_tristan_data_version_verify_v1():
-    pass
+    data_dir = repo_root / 'tests' / 'data' / 'tristan_v1'
+    file_names = ['flds.tot.041', 'param.041', 'prtl.tot.041', 'spect.041']
+
+    for name in file_names:
+        with h5py.File(data_dir / name, 'r') as file:
+            version = data_loading.__detect_tristan_data_version(file)
+            assert version == 1
 
 def test__detect_tristan_data_version_verify_v2():
-    pass
+    data_dir = repo_root / 'tests' / 'data' / 'tristan_v2' / 'single_directory'
+    file_names = ['flds.tot.00070', 'params.00070', 'prtl.tot.00070', 'spec.tot.00070']
+
+    for name in file_names:
+        with h5py.File(data_dir / name, 'r') as file:
+            version = data_loading.__detect_tristan_data_version(file)
+            assert version == 2
+
 
 def test__detect_tristan_data_version_incorrect_data():
-    pass
+    data_dir = repo_root / 'tests' / 'data' / 'unsupported_data'
+    file_names = ['flds.tot.041', 'param.041', 'prtl.tot.041', 'spect.041']
+
+    for name in file_names:
+        with h5py.File(data_dir / name, 'r') as file:
+            with pytest.raises(ValueError):
+                version = data_loading.__detect_tristan_data_version(file)
 
 def test__insert_directory_default():
     pass
