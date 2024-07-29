9f5864d4b18d0f271cfe7f9bd8702105d4924148
Bob Caddy <rcaddy@princeton.edu>
2024-07-29 15:23:12 -0400

    Add tests for `data_loading.__verify_file_path`

--- tests/test_data_loading.py
+++ tests/test_data_loading.py
@@ -47,8 +47,41 @@ def test__insert_directory_specified_location():
     fiducial_path = pathlib.Path('/dir1/dir2/new_dir/dir3/file.txt')
     assert test_path == fiducial_path
 
-def test__verify_file_path():
-    pass
+def test__verify_file_path_v1_data():
+    data_dir = repo_root / 'tests' / 'data' / 'tristan_v1'
+    file_names = ['flds.tot.041', 'param.041', 'prtl.tot.041', 'spect.041']
+
+    for name in file_names:
+        original_path = data_dir / name
+        found_path = data_loading.__verify_file_path(original_path, 'dataset_name')
+        assert found_path == original_path
+
+def test__verify_file_path_v2_data_single_dir():
+    data_dir = repo_root / 'tests' / 'data' / 'tristan_v2' / 'single_directory'
+    file_names = ['flds.tot.00070', 'params.00070', 'prtl.tot.00070', 'spec.tot.00070']
+
+    for name in file_names:
+        original_path = data_dir / name
+        found_path = data_loading.__verify_file_path(original_path, 'dataset_name')
+        assert found_path == original_path
+
+def test__verify_file_path_v2_data_multi_dir():
+    data_dir = repo_root / 'tests' / 'data' / 'tristan_v2' / 'standard_structure'
+    file_names = ['flds.tot.00070', 'params.00070', 'prtl.tot.00070', 'spec.tot.00070']
+
+    for name in file_names:
+        found_path = data_loading.__verify_file_path(data_dir / name, 'dataset_name')
+
+        if 'flds' in name:
+            fiducial_path = data_dir / 'flds' / name
+        elif 'prtl' in name:
+            fiducial_path = data_dir / 'prtl' / name
+        elif 'spec' in name:
+            fiducial_path = data_dir / 'spec' / name
+        else:
+            fiducial_path = data_dir / name
+
+        assert found_path == fiducial_path
 
 def test___handle_tristan_v2():
     pass
